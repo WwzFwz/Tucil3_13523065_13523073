d30f6e241c76560b49b6ed73e7e7b9c0e6005040
AlfianHanifFY <alfian.yustanto@gmail.com>
2025-05-17 22:02:51 +0700

    feat: add loadconfig from txt

---
+++ src/src/main/java/com/jawa/model/gameComponent/Piece.java
@@ -0,0 +1,109 @@
+package com.jawa.model.gameComponent;
+
+import javafx.scene.paint.Color;
+
+public class Piece {
+    private String id;
+    private final Position position;
+    private int length;
+    private boolean isHorizontal;
+    private Color color;
+    private boolean isPrimary;
+
+    public Piece(String id, int row, int col, int length, boolean isHorizontal, boolean isPrimary) {
+        this.id = id;
+        this.position = new Position(row, col);
+        this.length = length;
+        this.isHorizontal = isHorizontal;
+        this.isPrimary = isPrimary;
+
+        if (isPrimary) {
+            this.color = Color.PURPLE;
+        } else {
+            int hue = 0;
+            if (id.length() > 0 && id.charAt(0) >= 'A' && id.charAt(0) <= 'Z') {
+                int charValue = id.charAt(0) - 'A';
+                hue = (charValue * 360 / 26) % 360;
+            }
+            this.color = Color.hsb(hue, 0.7, 0.8);
+        }
+    }
+
+    public Piece(Piece other) {
+        this.id = other.id;
+        this.position = new Position(other.position);
+        this.length = other.length;
+        this.isHorizontal = other.isHorizontal;
+        this.color = other.color;
+        this.isPrimary = other.isPrimary;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getRow() {
+        return position.getRow();
+    }
+
+    public void setRow(int row) {
+        position.setRow(row);
+    }
+
+    public int getCol() {
+        return position.getCol();
+    }
+
+    public void setCol(int col) {
+        position.setCol(col);
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public boolean isHorizontal() {
+        return isHorizontal;
+    }
+
+    public boolean isPrimary() {
+        return isPrimary;
+    }
+
+    /**
+     * Mengembalikan Movement object setelah memindahkan piece
+     * (tanpa validasi posisi, lakukan validasi di Board)
+     */
+    public Movement move(String direction, int distance) {
+        switch (direction) {
+            case "R":
+                if (isHorizontal) {
+                    setCol(getCol() + distance);
+                }
+                break;
+            case "L":
+                if (isHorizontal) {
+                    setCol(getCol() - distance);
+                }
+                break;
+            case "U":
+                if (!isHorizontal) {
+                    setRow(getRow() - distance);
+                }
+                break;
+            case "D":
+                if (!isHorizontal) {
+                    setRow(getRow() + distance);
+                }
+                break;
+        }
+        return new Movement(getId(), direction, distance);
+    }
+
+    @Override
+    public String toString() {
+        return id + " at (" + getRow() + "," + getCol() + "), length=" + length +
+                ", " + (isHorizontal ? "horizontal" : "vertical") +
+                (isPrimary ? " (Primary)" : "");
+    }
+}
---
+++ src/src/main/java/com/jawa/model/gameComponent/Position.java
@@ -0,0 +1,52 @@
+package com.jawa.model.gameComponent;
+
+public class Position {
+    private int row;
+    private int col;
+
+    public Position(int r, int c) {
+        this.row = r;
+        this.col = c;
+    }
+
+    public Position(Position other) {
+        this.row = other.row;
+        this.col = other.col;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public void setCol(int col) {
+        this.col = col;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!(obj instanceof Position))
+            return false;
+        Position other = (Position) obj;
+        return this.row == other.row && this.col == other.col;
+    }
+
+    @Override
+    public int hashCode() {
+        return 31 * row + col;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + row + "," + col + ")";
+    }
+}
