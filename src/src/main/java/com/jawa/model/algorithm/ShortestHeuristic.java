646e546f8047cbc2484ee58dc442ad46754879d0
AlfianHanifFY <alfian.yustanto@gmail.com>
2025-05-20 01:08:04 +0700

    fix: shortest heuristic

--- src/src/main/java/com/jawa/model/algorithm/ShortestHeuristic.java
+++ src/src/main/java/com/jawa/model/algorithm/ShortestHeuristic.java
@@ -5,6 +5,7 @@ import com.jawa.model.gameComponent.Piece;
 import com.jawa.model.gameComponent.Position;
 
 public class ShortestHeuristic implements Heuristic {
+
     @Override
     public int estimate(Board board) {
         Piece primary = board.getPieces().get("P");
@@ -14,18 +15,31 @@ public class ShortestHeuristic implements Heuristic {
 
         int r = primary.getRow();
         int c = primary.getCol();
-
+        System.out.println("prim row " + r + " prim col " + c);
         if (primary.isHorizontal()) {
             int frontCol = c + primary.getLength() - 1;
-            // harus berada pada baris yang sama
-            if (r != exit.getRow())
-                return Integer.MAX_VALUE;
-            return Math.max(0, exit.getCol() - frontCol - 1); // jarak ke kanan
+
+            // Exit di kanan
+            if (r == exit.getRow() && exit.getCol() > frontCol)
+                return exit.getCol() - frontCol - 1;
+
+            // Exit di kiri
+            if (r == exit.getRow() && exit.getCol() < c)
+                return c - exit.getCol() - 1;
+
         } else {
             int frontRow = r + primary.getLength() - 1;
-            if (c != exit.getCol())
-                return Integer.MAX_VALUE;
-            return Math.max(0, exit.getRow() - frontRow - 1); // jarak ke bawah
+
+            // Exit di bawah
+            if (c == exit.getCol() && exit.getRow() > frontRow)
+                return exit.getRow() - frontRow - 1;
+
+            // Exit di atas
+            if (c == exit.getCol() && exit.getRow() < r)
+                return r - exit.getRow() - 1;
         }
+
+        return Integer.MAX_VALUE; // Exit tidak sejajar
     }
+
 }
